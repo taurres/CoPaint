eed1e94ab82e8571252506f68f1e98a2ca4a0f04
Gopal Krishna <mydigitalanjel@gmail.com>
2023-04-10 18:24:33 -0400

    update: add comments

--- FinalProject/src/source/network/server.d
+++ FinalProject/src/source/network/server.d
@@ -27,9 +27,13 @@ class Server {
     }
 
     ~this() {
+        // close server socket
+        this.listener.shutdown(SocketShutdown.BOTH);
         this.listener.close();
 
+        // close client sockets
         foreach (Socket client ; this.connectedClientList) {
+            client.shutdown(SocketShutdown.BOTH);
             client.close();
         }
     }
@@ -38,21 +42,29 @@ class Server {
         bool serverOnline = true;
         writeln("\nAwaiting client connections...");
 
+        // start accepting client connections on main thread
         while (serverOnline) {
+            // check if the max no. of clients are connected
             if (this.connectedClientList.length < this.maxNoOfClients) {
+                // accept new client connection requests
                 auto newClient = this.listener.accept();
                 newClient.send("... connected to the server");
 
+                // add new client on the connected client list
                 this.connectedClientList ~= newClient;
                 writeln("> client ", newClient.toHash(), " added to the client list...");
 
+                // run each of the connected clients on a new thread
                 new Thread({
                         this.runClient(newClient);
                     }).start();
             }
             else {
+                // accept new client momentarily to let them know max connections reached,
+                // then close the connection
                 auto newClient = this.listener.accept();
                 newClient.send("... cannot connect to the server, maximum no. of clients reached");
+                // close client socket
                 newClient.shutdown(SocketShutdown.BOTH);
                 newClient.close();
             }
@@ -63,17 +75,21 @@ class Server {
         bool clientRunning = true;
         writeln("client ", client.toHash(), " thread started...");
 
+        // initialize buffer to pass data between server and clients
         byte[1024] buffer;
 
         while (clientRunning) {
+            // recieve data from the client on current thread
             byte[] clientMessage = buffer[0 .. client.receive(buffer)];
 
             if (clientMessage.length == 0) {
+                // if no bytes were present in the client message, client probably disconnected
                 this.disconnectClient(client);
                 clientRunning = false;
                 break;
             }
             else if (clientMessage.length > 0) {
+                // if client message contained bytes, broadcast it to all clients except current
                 this.syncClients(client, clientMessage);
             }
         }
@@ -81,10 +97,14 @@ class Server {
 
     void disconnectClient(Socket client) {
         writeln("> client ", client.toHash(), " disconnected...");
+        // close current client socket
         client.shutdown(SocketShutdown.BOTH);
         client.close();
+
+        // get the index of the current client on the list of connected clients
         ulong i = getClientIdx(client);
         if (i >= 0) {
+            // if index is valid, remove the current client from the list of connected clients
             this.connectedClientList = this.connectedClientList[0 .. i] ~ this.connectedClientList[i + 1 .. $];
             writeln("... client ", client.toHash(), " removed from the client list");
         }
@@ -96,9 +116,11 @@ class Server {
     void syncClients(Socket client, byte[] clientMessage) {
         writeln("> client ", client.toHash(), " sent an update...");
 
+        // loop through all clients that are currently connected to the server
         foreach (Socket broadcastClient ; this.connectedClientList) {
             if (client.toHash() != broadcastClient.toHash()) {
                 writeln("Sending update to client ", broadcastClient.toHash(),  "...");
+                // send the current client's message to other clients
                 broadcastClient.send(cast(char[])dup(cast(const(byte)[]) clientMessage)); //TODO: understand/simplify this line
             }
         }
@@ -107,6 +129,7 @@ class Server {
     }
 
     ulong getClientIdx(Socket client) {
+        // linear search for current client
         foreach (i, Socket currClient ; this.connectedClientList) {
             if (client.toHash() == currClient.toHash()) {
                 return i;
