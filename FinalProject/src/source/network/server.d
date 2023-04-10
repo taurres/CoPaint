46824c6b8000076f1f26abf85d9dc3daed839696
Gopal Krishna <mydigitalanjel@gmail.com>
2023-04-10 17:58:15 -0400

    update: refactored server as per OOD

--- FinalProject/src/source/network/server.d
+++ FinalProject/src/source/network/server.d
@@ -1,75 +1,57 @@
 // @file server.d
 import std.stdio;
 import std.socket;
+import core.thread.osthread;
 
-void main() {
-    writeln("Starting server...");
-
-    writeln("\nCreating endpoint socket...");
-    auto listener = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
-    listener.bind(new InternetAddress("localhost", 8000));
-    scope(exit) listener.close();
-    writeln("Server is listening on port 8000...");
-
-    int maxNoOfClients = 3;
-    listener.listen(maxNoOfClients);
-
-    auto clientReadSet = new SocketSet();
+class Server {
+    Socket listener;
+    // initialize collection of sockets to keep track of clients
     Socket[] connectedClientList;
+    int maxNoOfClients;
 
-    byte[1024] buffer;
+    this(string host="localhost", ushort port=8000, int maxNoOfClients=3) {
+        writeln("Starting server...");
 
-    writeln("\nAwaiting client connections...");
-    bool serverOnline = true;
+        writeln("\nCreating endpoint socket...");
+        // creating socket for server to listen in and accept clients
+        // ProtocolType.TCP : SocketType.STREAM :: ProtocolType.UDP : SocketType.DGRAM
+        this.listener = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
 
-    while(serverOnline) {
-        clientReadSet.reset();
-        clientReadSet.add(listener);
+        // assign socket to a address with a port
+        this.listener.bind(new InternetAddress(host, port));
+        writeln("Server is listening on port 8000...");
 
-        foreach(Socket client ; connectedClientList) {
-            clientReadSet.add(client);
+        // enable socket to accept connections from another socket
+        this.maxNoOfClients = maxNoOfClients;
+        this.listener.listen(this.maxNoOfClients);
+    }
+
+    ~this() {
+        this.listener.close();
+
+        foreach (Socket client ; this.connectedClientList) {
+            client.close();
         }
+    }
 
-        if(auto updatedCount = Socket.select(clientReadSet, null, null)) {
-            foreach(i, Socket readClient ; connectedClientList) {
-                if(clientReadSet.isSet(readClient)) {
-                    auto clientMessage = buffer[0 .. readClient.receive(buffer)];
+    void run() {
+        bool serverOnline = true;
+        writeln("\nAwaiting client connections...");
 
-                    if(clientMessage.length == 0) {
-                        writeln("> client ", readClient.toHash(), " disconnected...");
-                        readClient.shutdown(SocketShutdown.BOTH);
-                        readClient.close();
-                        connectedClientList = connectedClientList[0 .. i] ~ connectedClientList[i + 1 .. $];
-                        writeln("... client ", readClient.toHash(), " removed from the client list");
-
-                    }
-                    else if(clientMessage.length > 0) {
-                        writeln("> client ", readClient.toHash(), " sent an update...");
-
-                        foreach(j, Socket writeClient ; connectedClientList) {
-                            if (i != j) {
-                                writeln("Sending update to client ", writeClient.toHash());
-                                writeClient.send(cast(char[])dup(cast(const(byte)[])clientMessage)); //TODO: understand/simplify this line
-                            }
-                        }
-
-                        writeln("... all clients synced");
-                    }
-                }
-            }
-        }
-
-
-        if(clientReadSet.isSet(listener)) {
-            if (connectedClientList.length < maxNoOfClients) {
-                auto newClient = listener.accept();
+        while (serverOnline) {
+            if (this.connectedClientList.length < this.maxNoOfClients) {
+                auto newClient = this.listener.accept();
                 newClient.send("... connected to the server");
 
-                connectedClientList ~= newClient;
+                this.connectedClientList ~= newClient;
                 writeln("> client ", newClient.toHash(), " added to the client list...");
+
+                new Thread({
+                        this.runClient(newClient);
+                    }).start();
             }
             else {
-                auto newClient = listener.accept();
+                auto newClient = this.listener.accept();
                 newClient.send("... cannot connect to the server, maximum no. of clients reached");
                 newClient.shutdown(SocketShutdown.BOTH);
                 newClient.close();
@@ -77,7 +59,65 @@ void main() {
         }
     }
 
-    foreach(Socket client ; connectedClientList) {
-        client.close();
+    void runClient(Socket client) {
+        bool clientRunning = true;
+        writeln("client ", client.toHash(), " thread started...");
+
+        byte[1024] buffer;
+
+        while (clientRunning) {
+            byte[] clientMessage = buffer[0 .. client.receive(buffer)];
+
+            if (clientMessage.length == 0) {
+                this.disconnectClient(client);
+                clientRunning = false;
+                break;
+            }
+            else if (clientMessage.length > 0) {
+                this.syncClients(client, clientMessage);
+            }
+        }
     }
+
+    void disconnectClient(Socket client) {
+        writeln("> client ", client.toHash(), " disconnected...");
+        client.shutdown(SocketShutdown.BOTH);
+        client.close();
+        ulong i = getClientIdx(client);
+        if (i >= 0) {
+            this.connectedClientList = this.connectedClientList[0 .. i] ~ this.connectedClientList[i + 1 .. $];
+            writeln("... client ", client.toHash(), " removed from the client list");
+        }
+        else {
+            writeln("... client", client.toHash(), " not found in the client list");
+        }
+    }
+
+    void syncClients(Socket client, byte[] clientMessage) {
+        writeln("> client ", client.toHash(), " sent an update...");
+
+        foreach (Socket broadcastClient ; this.connectedClientList) {
+            if (client.toHash() != broadcastClient.toHash()) {
+                writeln("Sending update to client ", broadcastClient.toHash(),  "...");
+                broadcastClient.send(cast(char[])dup(cast(const(byte)[]) clientMessage)); //TODO: understand/simplify this line
+            }
+        }
+
+        writeln("... all clients synced");
+    }
+
+    ulong getClientIdx(Socket client) {
+        foreach (i, Socket currClient ; this.connectedClientList) {
+            if (client.toHash() == currClient.toHash()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+void main() {
+    Server server = new Server();
+    server.run();
 }
