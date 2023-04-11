7f07b9bbbe6ed50ff11f8c736197f9f01dae1521
Gopal Krishna <mydigitalanjel@gmail.com>
2023-04-10 20:24:07 -0400

    updated: added comments to refactored client

--- FinalProject/src/source/network/client.d
+++ FinalProject/src/source/network/client.d
@@ -10,28 +10,37 @@ class Client {
         writeln("Starting client... ");
 
         writeln("\nCreating socket...");
+        // creating socket for client to send data and receive syncs from server
+        // ProtocolType.TCP : SocketType.STREAM :: ProtocolType.UDP : SocketType.DGRAM
         this.socket = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
 
+        // connecting client socket to the server
         this.socket.connect(new InternetAddress(host, port));
     }
 
     ~this() {
+        // close client socket
         this.socket.close();
     }
 
     void run() {
+        // initialize buffer to pass data between client and server
         byte[1024] buffer;
 
+        // receive connection ACK from the server
         writeln(cast(char[])(buffer[0 .. socket.receive(buffer)]));
         write("> ");
 
+        // new thread for listening to server for syncs
         new Thread({
                 this.listenForSyncs(socket, buffer);
             }).start();
 
+        // main thread is deelegated to send data to the server
         while(true) {
             foreach(line; stdin.byLine) {
                 write("> ");
+                // send data to the connected server socket
                 socket.send(line);
             }
         }
@@ -39,6 +48,7 @@ class Client {
 
     void listenForSyncs(Socket socket, byte[] buffer) {
         while(true) {
+            // listen to server's reply
             auto serverReply = buffer[0 .. socket.receive(buffer)];
 
             if(serverReply.length > 0) {
