8308cac90ecd1a4b5f8e51eb40702a4aa2ae8a8d
Jiaxi Wen <taurreswen@gmail.com>
2023-04-14 14:47:34 -0400

    update: integrate sdl, client, server

--- FinalProject/src/source/command.d
+++ FinalProject/src/source/command.d
@@ -1,15 +1,9 @@
-// Import D standard libraries
 import std.stdio;
 import std.string;
-import surface;
 import deque;
 
-// Load the SDL2 library
-import bindbc.sdl;
-import loader = bindbc.loader.sharedlib;
-
  interface Command { 
-    void execute();
+    // void execute();
   }
 
   // DRAW
@@ -18,7 +12,7 @@ import loader = bindbc.loader.sharedlib;
   // ERASE
 
   class EraseCommand : Command {
-    Surface surface;
+    char[16] name;
     int xPos;
     int yPos;
     int brushSize;
@@ -26,8 +20,8 @@ import loader = bindbc.loader.sharedlib;
     ubyte g = 0;
     ubyte b = 0;
 
-    this(Surface surface, int xPos, int yPos, int brushSize){
-        this.surface = surface;
+    this(int xPos, int yPos, int brushSize){
+        this.name = "Erase";
         this.xPos = xPos;
         this.yPos = yPos;
         this.brushSize = brushSize;
@@ -37,24 +31,22 @@ import loader = bindbc.loader.sharedlib;
         // Not sure what to do here
     }
 
-    void execute(){
-        surface.changePixel(xPos, yPos, r, g, b);
-    }
+    // void execute(){
+    //     surface.changePixel(xPos, yPos, r, g, b);
+    // }
 
   }
 
 
   //Update pixel class
   class DrawCommand : Command {
-    Surface* surface;
-    SDL_Surface* imgSurface;
+    char[16] name;
     int xPos;
     int yPos;
     int brushSize;
 
-    this(Surface* surface, SDL_Surface* imgSurface, int xPos, int yPos, int brushSize){
-        this.surface = surface;
-        this.imgSurface = imgSurface;
+    this(int xPos, int yPos, int brushSize){
+        this.name = "Draw";
         this.xPos = xPos;
         this.yPos = yPos;
         this.brushSize = brushSize;
@@ -64,29 +56,29 @@ import loader = bindbc.loader.sharedlib;
         // Not sure what to do here
     }
 
-    void execute(){
-      // Loop through and update specific pixels
-        for(int w=-brushSize; w < brushSize; w++){
-          for(int h=-brushSize; h < brushSize; h++){
-              surface.UpdateSurfacePixel(imgSurface, xPos+w, yPos+h);
-          }
-      }
-    }
+    // void execute(){
+    //   // Loop through and update specific pixels
+    //     for(int w=-brushSize; w < brushSize; w++){
+    //       for(int h=-brushSize; h < brushSize; h++){
+    //           surface.UpdateSurfacePixel(imgSurface, xPos+w, yPos+h);
+    //       }
+    //   }
+    // }
 
   }
 
   class UndoCommand : Command {
     // Deque undo_deque = new Deque();
-    void execute() {
-      // Pop back from global command deque
-      // Push into redo stack
-    }
+    // void execute() {
+    //   // Pop back from global command deque
+    //   // Push into redo stack
+    // }
   }
 
   class RedoCommand : Command {
     // T[] redo_stack;
-    void execute() {
-      // Pop from redo stack
-      // Push into global command stack
-    }
+    // void execute() {
+    //   // Pop from redo stack
+    //   // Push into global command stack
+    // }
   }
