6ff7de07347a10cf025cba9efa3d0c322fc5c5a8
Muskaan Nandu <nandumuskaan@gmail.com>
2023-04-17 16:42:38 -0400

    update: undo command implemented

--- FinalProject/src/source/command.d
+++ FinalProject/src/source/command.d
@@ -5,16 +5,23 @@ import network.packet;
 import surface;
 import constants;
 
+// Load the SDL2 library
+import bindbc.sdl;
+import loader = bindbc.loader.sharedlib;
+
  interface Command {
     /**
      * Executes the command on the given surface.
      */ 
     void execute(Surface* surface);
 
-    // TODO - add unexecute method
+    /** 
+    * Unexecutes the command on the given surface.
+    */
+    void unexecute(Surface* surface);
 
     /**
-     * Covert this command to a packet.
+     * Convert this command to a packet.
      */
     Packet toPacket();
 
@@ -23,11 +30,12 @@ import constants;
       */
     static Command fromPacket(Packet packet) {
       if (packet.commandId == DRAW_COMMAND_ID) {
-        return new DrawCommand(packet.x, packet.y, packet.brushSize);
+        return new DrawCommand(packet.x, packet.y, packet.brushSize, packet.pre_r, packet.pre_g, packet.pre_b);
       } else if (packet.commandId == ERASE_COMMAND_ID) {
         return new EraseCommand(packet.x, packet.y, packet.brushSize);
       } else if (packet.commandId == UNDO_COMMAND_ID) {
-        return new UndoCommand();
+        //create a draw commad and send it to undo
+        return new UndoCommand(packet.prevCommand); 
       } else if (packet.commandId == REDO_COMMAND_ID) {
         return new RedoCommand();
       } else {
@@ -42,17 +50,23 @@ import constants;
   // REDO
   // ERASE
 
-    //Update pixel class
+  //Update pixel class
   class DrawCommand : Command {
     int commandId = DRAW_COMMAND_ID;
     int xPos;
     int yPos;
     int brushSize;
+    ubyte pre_r;
+    ubyte pre_g;
+    ubyte pre_b;
 
-    this(int xPos, int yPos, int brushSize){
+    this(int xPos, int yPos, int brushSize, ubyte pre_r = 0, ubyte pre_g = 0, ubyte pre_b = 0){
         this.xPos = xPos;
         this.yPos = yPos;
         this.brushSize = brushSize;
+        this.pre_r = pre_r;
+        this.pre_g = pre_g;
+        this.pre_b = pre_b;
     }
 
     ~this(){
@@ -61,6 +75,10 @@ import constants;
 
     void execute(Surface* surface){
       // TODO capture previous state
+      SDL_Color pixel = surface.pixelAt(xPos,yPos);
+      pre_r = pixel.r;
+      pre_g = pixel.g;
+      pre_b = pixel.b;
 
       // Loop through and update specific pixels
         for(int w=-brushSize; w < brushSize; w++){
@@ -68,14 +86,12 @@ import constants;
               surface.UpdateSurfacePixel(xPos+w, yPos+h);
           }
       }
-    
-  
     }
 
     // TODO
-    // void unexecute(Surface* surface){
-    //     surface.changePixel(xPos, yPos, pre_r, pre_g, b);
-    // }
+    void unexecute(Surface* surface){
+        surface.changePixel(xPos, yPos, pre_r, pre_g, pre_b);
+    }
     
 
     Packet toPacket() {
@@ -100,11 +116,17 @@ import constants;
     ubyte r = 0; // assign background color to r,g,b
     ubyte g = 0;
     ubyte b = 0;
+    ubyte pre_r;
+    ubyte pre_g;
+    ubyte pre_b;
 
-    this(int xPos, int yPos, int brushSize){
+    this(int xPos, int yPos, int brushSize, ubyte pre_r = 0, ubyte pre_g = 0, ubyte pre_b = 0){
         this.xPos = xPos;
         this.yPos = yPos;
         this.brushSize = brushSize;
+        this.pre_r = pre_r;
+        this.pre_g = pre_g;
+        this.pre_b = pre_b;
     }
 
     ~this(){
@@ -112,6 +134,10 @@ import constants;
     }
 
     void execute(Surface* surface){
+        SDL_Color pixel = surface.pixelAt(xPos,yPos);
+        pre_r = pixel.r;
+        pre_g = pixel.g;
+        pre_b = pixel.b;
         surface.changePixel(xPos, yPos, r, g, b);
     }
 
@@ -126,15 +152,39 @@ import constants;
       p.brushSize = this.brushSize;
       return p;
     }
+
+    // TODO
+    void unexecute(Surface* surface){
+        surface.changePixel(xPos, yPos, pre_r, pre_g, pre_b);
+    }
   }
 
   // TODO
   class UndoCommand : Command {
     int commandId = UNDO_COMMAND_ID;
-    // Deque undo_deque = new Deque();
-    void execute(Surface* surface) {
-    //  TODO call unexecute of draw command
+    Command prevCommand;
+    ubyte pre_r;
+    ubyte pre_g;
+    ubyte pre_b;
+
+    this(Command prevCommand){
+      this.prevCommand = prevCommand;
     }
+
+    void execute(Surface* surface) {
+        SDL_Color pixel = surface.pixelAt(prevCommand.xPos,prevCommand.yPos);
+        pre_r = pixel.r;
+        pre_g = pixel.g;
+        pre_b = pixel.b;
+        //  TODO call unexecute of draw commanD
+        prevCommand.unexecute(surface);
+    }
+
+    // TODO - might need for redo
+    void unexecute(Surface* surface){
+        surface.changePixel(prevCommand.xPos, prevCommand.yPos, pre_r, pre_g, pre_b);
+    }
+
     Packet toPacket() { return Packet();}
   }
 
@@ -145,6 +195,10 @@ import constants;
     void execute(Surface* surface) {
     //   // Pop from redo stack
     //   // Push into global command stack
+    }
+
+    void unexecute(Surface* surface){
+
     }
     Packet toPacket() { return Packet();}
   }
