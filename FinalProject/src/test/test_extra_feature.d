87fa115ccecd065def6a63a918ff870241553aa9
clselva <selva.c@northeastern.edu>
2023-04-18 20:35:36 -0400

    fixed extra feature test file

--- FinalProject/src/test/test_extra_feature.d
+++ FinalProject/src/test/test_extra_feature.d
@@ -16,28 +16,18 @@ unittest{
     assert( myApp.getBrushSize() == 3, "Brushsize is incorrect");
     myApp.increase_brush();
     assert( myApp.getBrushSize() == 4, "Brushsize is incorrect");
-
-    writeln("Unit test for extra feature passed: Testing if the increase_brush function works");
-}
-/**
-//@("Unit Test 2 for Extra Feature")
-unittest{
-    SDLApp myApp = new SDLApp();
     myApp.increase_brush();
-    myApp.increase_brush();
-    myApp.increase_brush();
-    myApp.increase_brush();
-    myApp.increase_brush();
-    assert( myApp.getBrushSize() == 6, "Brushsize is incorrect");
-    myApp.decrease_brush();
     assert( myApp.getBrushSize() == 5, "Brushsize is incorrect");
     myApp.decrease_brush();
     assert( myApp.getBrushSize() == 4, "Brushsize is incorrect");
     myApp.decrease_brush();
     assert( myApp.getBrushSize() == 3, "Brushsize is incorrect");
+    myApp.decrease_brush();
+    assert( myApp.getBrushSize() == 2, "Brushsize is incorrect");
+    myApp.decrease_brush();
+    assert( myApp.getBrushSize() == 1, "Brushsize is incorrect");
 
-    writeln("Unit test for extra feature passed: Testing if the decrease_brush function works");
-    
+    writeln("Unit test for extra feature passed: Testing if the increase_brush and decrease_brush functions work");
 }
-*/
+
 
